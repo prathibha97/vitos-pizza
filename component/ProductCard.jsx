5b20e2e7fac23849dd7708dd4f91733e0672e1f5
prathibha <prsthibha@gmail.com>
2022-10-06 19:02:07 +0530

    Add ProductCard component and styles

---
+++ component/ProductCard.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import style from '../styles/ProductCard.module.css'
+
+const ProductCard = () => {
+    return (
+        <div className={style.container}>
+            <Image src='/images/pizza.png' width={500} height={500} alt='' />
+            <h1 className={style.title}>FIORI DI ZUCCA</h1>
+            <span className={style.price}>$19.90</span>
+            <p className={style.desc}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            </p>
+        </div>
+    )
+}
+
+export default ProductCard
