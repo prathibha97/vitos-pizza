8d4cfc5684841c68c0484bc075164efe62b12b29
prathibha <prsthibha@gmail.com>
2022-10-07 18:38:37 +0530

    Create cart reducer

---
+++ redux/cartSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    products: [],
+    quantity: 0,
+    total: 0,
+  },
+  reducers: {
+    addProduct: (state, action) => {
+      const { payload } = action;
+      state.products.push(payload);
+      state.total += payload.price * payload.quantity;
+      state.quantity += 1;
+    },
+    reset: (state) => {
+      state = initialState;
+    },
+  },
+});
+
+export const { addProduct, reset } = cartSlice.actions;
+
+export default cartSlice.reducer;
